ffcf93c8538561b45fc32c67a84eb7693390754d
Jozef Cechovsky <cechovsky.jozef@gmail.com>
2014-04-26 22:53:12 +0200

    inverse of matrix caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R contains functions to cache support of matrix inversion
 
-## Write a short comment describing this function
+## makeCacheMatrix is encapsulation of matrix with its inversion
+## input : matrix
+## output : list of operations
+##  $get(x) - get matrix
+##  $set(x) - set matrix
+##  $getInversion(x) - get inversion of matrix
+##  $setInversion(x) - set inversion of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## throw error if matrix is not square
+  if(ncol(x) != nrow(x)){
+    stop("matrix has to be square")
+  }
+  inversion <- NULL
+  set <- function(y) {
+    x <<-y
+    inversion <- NULL
+  }
+  get <- function() x
+  setInversion <- function(i) inversion <<- i
+  getInversion <- function() inversion
+  list(set = set, get = get, 
+       setInversion = setInversion, getInversion = getInversion)
 }
 
-
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve resolves getting of matrix inverse 
+## input : cacheMatrix (created with function above)
+## output : inversion of matrix
+cacheSolve <- function(x, ...) {        
+  inversion <- x$getInversion()
+  if(!is.null(inversion)){
+    return(inversion)   
+  }
+  inversion <- solve(x$get())
+  x$setInversion(inversion)
+  inversion
 }
